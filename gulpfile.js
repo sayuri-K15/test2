5066888c3dc6c4b124f512c4b15b06d86a00fbd8
zd3H04 <zd15shirokuma@gmail.com>
2019-12-25 10:12:50 +0900

    add js task2

--- gulpfile.js
+++ gulpfile.js
@@ -17,21 +17,21 @@ gulp.task('delSass',function() {
 });
 
 gulp.task('copyHtml',function() {
-  return gulp.src(['assets/**/*.html'])
+  return gulp.src(['_assets/**/*.html'])
   .pipe(plumber())
   .pipe(gulp.dest('html/'))
   .pipe(browserSync.stream());
 });
 
 gulp.task('copyJs',function() {
-  return gulp.src(['assets/js/*.js'])
+  return gulp.src(['_assets/js/*.js'])
   .pipe(plumber())
   .pipe(gulp.dest('html/js/'))
   .pipe(browserSync.stream());
 });
 
 gulp.task('sass',function() {
-  return gulp.src(['assets/sass/*.scss'])
+  return gulp.src(['_assets/sass/*.scss'])
   .pipe(plumber())
   .pipe(sass({outputStyle: 'compressed'}))
   .pipe(gulp.dest('html/css/'))
@@ -44,7 +44,7 @@ gulp.task('default',['delHtml','delJs','delSass','copyHtml','copyJs','sass'], fu
       baseDir: 'html'
     }
   });
-  gulp.watch(['assets/**/*.html'],['delHtml','copyHtml']);
-  gulp.watch(['assets/js/*.js'],['delJs','copyJs']);
-  gulp.watch(['assets/sass/*.scss'],['delSass','sass']);
+  gulp.watch(['_assets/**/*.html'],['delHtml','copyHtml']);
+  gulp.watch(['_assets/js/*.js'],['delJs','copyJs']);
+  gulp.watch(['_assets/sass/*.scss'],['delSass','sass']);
 })
